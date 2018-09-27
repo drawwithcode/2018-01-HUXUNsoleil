9bc1fa5c291805b739db02ef39839a9066b65d1f
HUXUNsoleil <43408081+HUXUNsoleil@users.noreply.github.com>
2018-09-28 01:36:39 +0200

    HUXUN-roboter

--- sketch.js
+++ sketch.js
@@ -1,23 +1,58 @@
+function preload(){
+  // put preload code here
+
+}
+
 function setup() {
-	// define artboard size
-	createCanvas(500,500);
-	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+createCanvas(800, 800);
+
 }
 
 function draw() {
-	background(204);
+  background(120,120,120,100);
 
-	//Neck
-	line(250, 285, 250, 345);
+  push();
+    translate(mouseX/2,mouseY/2+400);
+    fill(175,175,175);
+    arc(0, 0, 50, 50, 0, 180, PI);
+    fill(0,0,0);
+    arc(0, 0, 25, 25, 0, 180, PI);
+    fill(255,255,255);
+    arc(5, 5, 12, 12, 0, 180, PI);
+    pop();
+    push();
+      translate(mouseX/2+805/2,mouseY/2+805/2);
+      fill(175,175,175);
+      arc(0, 0, 50, 50, 0, 180, PI);
+      fill(0,0,0);
+      arc(0, 0, 25, 25, 0, 180, PI);
+      fill(255,255,255);
+      arc(5, 5, 12, 12, 0, 180, PI);
+      pop();
 
-	//Wheel
-	ellipse(250, 465, 70, 70);
+ stroke(0);
+ // xP1, yP1, xA1, yA1, xA2, yA2, xP2, yP2
+ bezier(0,800,mouseX,mouseY,mouseX+5,mouseY+5,800,800)
+ push();
+   translate(mouseX/2+805/4,mouseY/2+805/2);
+   fill(230,97,40);
+   rotate(mouseX / 100);
+   if(mouseY>=100){
 
-	//Body
-	rect(200, 325, 100, 140);
-
-	//Head
-	ellipse(250, 240, 90, 90);
+   polygon(0,0,4000/mouseY,3);
+   }
+   else{
+     polygon(0,0,40,3);
+   }
+   pop();
+}
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
